8517765a9ea6d8ef08ff3df85da7ece79b5640c6
NessaAz <vanessa.azenwa@gmail.com>
2022-10-31 20:41:14 +0300

    modify sorting

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
 })
 export class ProductsHeaderComponent implements OnInit {
+  
+  //products header variables
   sort = 'descending';
+  itemsShowCount = 12;
+  @Output() columnsCountChange = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit(): void {
@@ -13,5 +18,13 @@ export class ProductsHeaderComponent implements OnInit {
 
   onSortUpdated(newSort: string): void{
     this.sort = newSort;
+  }
+
+  onItemUpdated(count: number): void{
+    this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colsNum: number): void{
+    this.columnsCountChange.emit(colsNum);
   }
 }
