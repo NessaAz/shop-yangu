19d2eb70a1f7e30665b8003701bb28ee72d8170c
NessaAz <vanessa.azenwa@gmail.com>
2022-11-06 21:14:30 +0300

    create the rows height property

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+//object we'll use to map the height
+const ROWS_HEIGHT: { [id:number]: number} = { 
+  1:400, 
+  3:335, 
+  4:350
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,6 +14,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   cols = 3;
   category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
 
   constructor() { }
 
@@ -15,6 +23,7 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void{
     this.cols = colsNum;
+    this. rowHeight = ROWS_HEIGHT[this.cols];
   }
   onshowCategory(newCategory: string): void {
     this.category = newCategory;
